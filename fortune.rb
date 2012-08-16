2e1378c108397ba4ab68cdf0d33d6fdaa2bac544
Elaine Marino <elaine.c.marino@gmail.com>
2012-08-15 20:07:02 -0600

    Revised Fortunes to be strings.

--- fortune.rb
+++ fortune.rb
@@ -1,7 +1,10 @@
 class Fortune
 
   def initalize
-    @fortune = ["1", "2", "3"]
+    @fortune = ["This is a great day!",
+                "Dark clouds are above.",
+                "Try again tomorrow.",
+               ]
   end
 
   def play
